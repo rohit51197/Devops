a1f01359f4d98a9582ff498ff524389adcfc030e
rohit51197 <46321947+rohit51197@users.noreply.github.com>
2020-01-22 14:01:11 +0530

    Add files via upload

---
+++ sample1/src/sample1/Main1.java
@@ -0,0 +1,14 @@
+package sample1;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		a=50;
+		b=20;
+		c=a+b;
+		System.out.println("the value of c is:"+c);
+
+	}
+
+}
